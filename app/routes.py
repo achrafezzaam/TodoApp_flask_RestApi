c45f6cf59d2754f109754db0d077835364abbf40
achrafezzaam <achraf.ezzaam@gmail.com>
2022-10-19 16:29:46 +0100

    feat: adding an index page
    - Adding an index page that's ewtending from a base.html
    - Adding the static folder where the styles and scripts will
    be hosted

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    user = { "username": "Achraf" }
+    return render_template("index.html", title="Dashboard", user=user)
