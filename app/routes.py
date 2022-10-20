62c831cc847e9a336789ce34130008ef970df4f2
achrafezzaam <achraf.ezzaam@gmail.com>
2022-10-20 10:58:24 +0100

    feat: Adding a login functionality
    - Creating the database
    - Writing the login view in the routes file and
    the associated form in the forms file
    - Creating the user model
    - Adding the db file to the gitignore file

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,36 @@
+from flask_login import current_user, login_required, login_user
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask_login import logout_user
+from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { "username": "Achraf" }
-    return render_template("index.html", title="Dashboard", user=user)
+
+    return render_template("index.html", title="Dashboard")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
+    return render_template("login.html", title="Login page", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    
+    return redirect(url_for('index'))
+
+# @app.route('dashboard')
+# @login_required
+# def dashboard():
