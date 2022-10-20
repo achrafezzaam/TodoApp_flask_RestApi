86e8b3cbe1c56922d43cfc11d48a793b42e6f7c7
achrafezzaam <achraf.ezzaam@gmail.com>
2022-10-20 12:50:41 +0100

    feat: Redirection when login and adding account creation - Renaming the login route to signin - The signin route redirects the user to his dashboard - Adding an account creation route with it's associated form and page

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Log in')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Enter a username', validators=[DataRequired()])
+    password_1 = PasswordField('Enter a password', validators=[DataRequired()])
+    password_2 = PasswordField('Retype your password', \
+        validators=[DataRequired(),EqualTo('password_1')])
+    submit = SubmitField('Create')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('This username is already in use. Try another one')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
+from crypt import methods
 from flask_login import current_user, login_required, login_user
-from app import app
-from flask import render_template, redirect, url_for
+from app import app, db
+from flask import render_template, redirect, url_for, request
 from flask_login import logout_user
-from app.forms import LoginForm
+from werkzeug.urls import url_parse
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 @app.route('/')
@@ -11,19 +13,41 @@ def index():
 
     return render_template("index.html", title="Dashboard")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    # Checkiing if the user is authenticated
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
+
+    # Login form handling. If the user isn't found or the password is wrong
+    # The user is returned to the login page
+    # TODO show a message to the user if one of the above creteria is wrong
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            return redirect(url_for('login'))
+            return redirect(url_for('signin'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc!='':
+            next_page = url_for('dashboard')
+        return redirect(next_page)
 
-    return render_template("login.html", title="Login page", form=form)
+    return render_template("signin.html", title="signin page", form=form)
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password_1.data
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('signin'))
+
+    return render_template("signup.html", title="Create an account", form=form)
 
 @app.route('/logout')
 def logout():
@@ -31,6 +55,9 @@ def logout():
     
     return redirect(url_for('index'))
 
-# @app.route('dashboard')
-# @login_required
-# def dashboard():
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    user = User.query.filter_by(username=current_user.username).first()
+
+    return render_template("dashboard.html", title="Dashboard", username=user.username)
